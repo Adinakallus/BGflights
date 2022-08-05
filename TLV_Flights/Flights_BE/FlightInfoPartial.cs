9df03d3219e1444c21908ae3416f5f0d52f3e9f6
shirelk <shkadosh@g.jct.ac.il>
2022-08-05 18:07:59 +0300

    flight info (using JSON)

---
+++ TLV_Flights/Flights_BE/FlightInfoPartial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flights_BE
+{
+    internal class FlightInfoPartial
+    {
+        public int Id { get; set; }
+        public string SourceId { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public String Source { get; set; }
+        public String Destination { get; set; }
+        public String FlightCode { get; set; }
+    }
+}
