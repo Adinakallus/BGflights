6d72fd00327e4122ac0e41227f28b8ccf69cca6c
Adina Kallus <kallus@g.jct.ac.il>
2022-08-19 02:45:28 +0300

    BL except last few user functions

---
+++ TLV_Flights/Flights_BL/UserExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Flights_BL
+{
+    public class UserExistsException : Exception
+    {
+        public String UserName;
+
+        public UserExistsException(String username) : base() => UserName = username;
+
+
+        public UserExistsException(String username, string? message) : base(message) => UserName = username;
+
+        public UserExistsException(String username,String? message, Exception? innerException) : base(message, innerException) => UserName = username;
+
+        public override string ToString() => base.ToString() + $"Username:{UserName} already exists";
+
+    }
+}
