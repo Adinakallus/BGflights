fc8868cf52b950b10494a9ea5650d96483b0e859
Adina Kallus <kallus@g.jct.ac.il>
2022-08-29 22:02:07 +0300

    ErevChag model and uc

---
+++ TLV_Flights/Flights_PL/Model/ErevChagModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flights_BE;
+using Flights_BL;
+
+namespace Flights_PL.Model
+{
+    public class ErevChagModel
+    {
+        private readonly BAL _bal;
+        public ErevChagModel()
+        {
+            _bal = new BAL();
+        }
+        public async Task<bool> isErevChag()
+        {
+            return await _bal.isErevChag();
+        }
+
+    }
+}
