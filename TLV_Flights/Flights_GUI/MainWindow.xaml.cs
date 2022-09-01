377c6bfc7e851b39fbfbd1cdc25dedb05260a933
Adina Kallus <kallus@g.jct.ac.il>
2022-09-01 21:38:48 +0300

    Opened new UI project and the designer works

--- TLV_Flights/Flights_GUI/MainWindow.xaml.cs
+++ TLV_Flights/Flights_GUI/MainWindow.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Flights_PL
+namespace Flights_GUI
 {
     /// <summary>
-    /// Interaction logic for app.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class app 
+    public partial class MainWindow : Window
     {
-        public app()
+        public MainWindow()
         {
             InitializeComponent();
         }
---
+++ TLV_Flights/Flights_GUI/Model/FlightInfoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flights_BE;
+using Flights_BL;
+
+namespace Flights_PL.Model
+{
+    public class FlightInfoModel
+    {
+        private readonly BAL _bal;
+        public FlightInfoModel(BAL bal)
+        {
+            _bal = new ();
+        }   
+        public async Task<FlightInfo> GetFlightInfo(FlightInfoPartial flight)
+        {
+            return await _bal.GetFlightInfo(flight);
+        }
+    }
+}
