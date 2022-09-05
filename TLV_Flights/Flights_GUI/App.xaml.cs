93ed04aefa2031ca80461f257bc942590e0881a0
shirelk <shkadosh@g.jct.ac.il>
2022-09-05 22:46:18 +0300

    bing maps - with a problem

--- TLV_Flights/Flights_GUI/App.xaml.cs
+++ TLV_Flights/Flights_GUI/App.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Wpf.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Windows.Services.Maps;
 
 namespace Flights_GUI
 {
@@ -13,5 +15,6 @@ namespace Flights_GUI
     /// </summary>
     public partial class App : Application
     {
+        public static string token = "AuZE4zWo5xFW5WsMQOoYlKQ9z_MX1VN1TD0To1XhJMRqDuTi99wIMfJdyoNX4xYd";
     }
 }
--- TLV_Flights/Flights_GUI/MainWindow.xaml.cs
+++ TLV_Flights/Flights_GUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Wpf.UI.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.Services.Maps;
+using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+
 
 namespace Flights_GUI
 {
@@ -23,6 +27,20 @@ namespace Flights_GUI
         public MainWindow()
         {
             InitializeComponent();
+            
+            var mapControl = new MapControl();
+            mapControl.Loaded += MapControl_Loaded;
+            mapControl.MapServiceToken = App.token;
+            MapGrid.Children.Add(mapControl);
+        }
+
+        private async void MapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            //by Israel latitude and longitude
+            BasicGeoposition geoposition = new BasicGeoposition() { Latitude = 31.0461, Longitude = 34.8516 };
+            var center = new Geopoint(geoposition);
+
+            ((MapControl)sender).TrySetViewAsync(center , 4);
         }
     }
 }
