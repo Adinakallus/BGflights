a3d745d7b997c6520034a78c6367219575485e18
Adina Kallus <kallus@g.jct.ac.il>
2022-09-06 22:45:09 +0300

    ViewModelBase satrted Login vm and commnd

--- TLV_Flights/Flights_GUI/UserControls/LoginUserControl.xaml.cs
+++ TLV_Flights/Flights_GUI/UserControls/LoginUserControl.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using Flights_GUI.ViewModel;
 namespace Flights_GUI.UserControls
 {
     /// <summary>
