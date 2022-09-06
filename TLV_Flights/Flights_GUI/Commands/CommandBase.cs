c353bfcf1b7954d805b3ab99adfd59d0e5c7ff7f
Adina Kallus <kallus@g.jct.ac.il>
2022-09-06 20:32:13 +0300

    CommandBase

---
+++ TLV_Flights/Flights_GUI/Commands/CommandBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Flights_GUI.Commands
+{
+    public abstract class CommandBase:ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        public virtual bool CanExecute(object paramater)
+        {
+            return true;
+        }
+        public abstract void Execute(object paramater);
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());   
+        }
+    }
+}
